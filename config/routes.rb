d4f5c0673a622ec9bdd7cfe5bb3f736adfbb0fff
Guillaume de Malzac <guillaume.de.malzac@gmail.com>
2023-02-22 15:28:00 +0100

    wesh

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :restaurants do
+    resources :reviews, only: [:index, :new, :create]
+  end
+  resources :reviews, only: [:show, :edit, :update, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
